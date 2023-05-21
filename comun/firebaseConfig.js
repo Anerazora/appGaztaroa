5f562787d6abf550c63bc824583e900a58120a45
Aro <Aro@Laya2-2.local>
2023-05-21 23:02:39 +0200

    Autentificacion en Firebase

---
+++ comun/firebaseConfig.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD19faNvU8hVoMpSzLppbHxMJy8PfBdHbY",
+    authDomain: "appgaztaroa-53ec5.firebaseapp.com",
+    databaseURL: "https://appgaztaroa-53ec5-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "appgaztaroa-53ec5",
+    storageBucket: "appgaztaroa-53ec5.appspot.com",
+    messagingSenderId: "750197807379",
+    appId: "1:750197807379:web:ac15b99608637a443762e6",
+    measurementId: "G-5NHR2QKDVL"
+};
+
